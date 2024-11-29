69010fc0effa0ad0bf1f3667253fffbd311d1f7f
Alexandr Lukanev <lukanevteach@gmail.com>
2024-11-29 17:45:48 +0300

    added areas

---
+++ prisma/migrations/20241231163647_update_area_seed_data/migration.sql
@@ -0,0 +1,12 @@
+INSERT INTO "areas" (id, number, lat, lon, name, status, "createdAt", "updatedAt")
+VALUES
+    (gen_random_uuid(), 14, 59.8971, 30.5108, 'Кудрово', 'PUBLISHED', NOW(), NOW()),
+    (gen_random_uuid(), 15, 59.9500, 30.4530, 'Мурино', 'PUBLISHED', NOW(), NOW()),
+    (gen_random_uuid(), 16, 60.0367, 30.3545, 'Парнас', 'PUBLISHED', NOW(), NOW()),
+    (gen_random_uuid(), 17, 59.7167, 30.3949, 'Пушкинский', 'PUBLISHED', NOW(), NOW()),
+    (gen_random_uuid(), 18, 59.8057, 30.3856, 'Шушары', 'PUBLISHED', NOW(), NOW()),
+    (gen_random_uuid(), 19, 59.9300, 30.6000, 'Янино', 'PUBLISHED', NOW(), NOW()),
+    (gen_random_uuid(), 20, 60.1870, 29.7298, 'Курортный', 'PUBLISHED', NOW(), NOW()),
+    (gen_random_uuid(), 21, 59.9957, 29.7660, 'Кронштадский', 'PUBLISHED', NOW(), NOW()),
+    (gen_random_uuid(), 22, 59.7507, 30.5760, 'Колпинский', 'PUBLISHED', NOW(), NOW()),
+    (gen_random_uuid(), 23, 59.8830, 29.9098, 'Петродворцовый', 'PUBLISHED', NOW(), NOW());
