ea9f974c3490a66b0a903869f88bbd517fc0818c
Alexandr Lukanev <lukanevteach@gmail.com>
2025-03-30 22:04:25 +0300

    fix mail service

--- src/services/mail.service.ts
+++ src/services/mail.service.ts
@@ -3,7 +3,39 @@ import * as nodemailer from 'nodemailer';
 
 @Injectable()
 export class MailService {
-    async sendMail(email: string, subject: string, text: string): Promise<any> {
+
+  private transporter: nodemailer.Transporter;
+
+  constructor() {
+      this.transporter = nodemailer.createTransport({
+          host: 'smtp.mail.ru',
+          port: 465, // Mail.ru требует 465 с SSL или 587 с STARTTLS
+          secure: true, // true для 465, false для 587
+          auth: {
+              user: process.env.SMTP_EMAIL,  // Укажите ваш email на Mail.ru
+              pass: process.env.SMTP_PASSWORD // Пароль или специальный пароль приложения
+          }
+      });
+  }
+
+  async sendMail(email: string, subject: string, text: string): Promise<void> {
+      const mailOptions = {
+          from: process.env.SMTP_EMAIL,
+          to: email,
+          subject: subject,
+          text: text,
+      };
+
+      try {
+          console.log('Отправка письма:', mailOptions);
+          await this.transporter.sendMail(mailOptions);
+          console.log('Письмо успешно отправлено!');
+      } catch (error) {
+          console.error('Ошибка при отправке письма:', error);
+      }
+  }
+  
+    async sendMailYandex(email: string, subject: string, text: string): Promise<any> {
         const transporter = nodemailer.createTransport({
             service: 'Yandex',
             auth: {
