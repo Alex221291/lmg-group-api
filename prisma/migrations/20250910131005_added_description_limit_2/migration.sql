e77a3209e6b92f4af1b0aef5bdc3004c1768ae74
Alexandr Lukanev <lukanevteach@gmail.com>
2025-09-10 16:10:43 +0300

    added limit 2

---
+++ prisma/migrations/20250910131005_added_description_limit_2/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "builds" ALTER COLUMN "wDescription" SET DATA TYPE VARCHAR(10000);
+
+-- AlterTable
+ALTER TABLE "category_area" ALTER COLUMN "description" SET DATA TYPE VARCHAR(10000);
+
+-- AlterTable
+ALTER TABLE "sections" ALTER COLUMN "description" SET DATA TYPE VARCHAR(10000);
